9c25d8e5f1fa323ad8e464b11b270c32f6eaca12
Sameer Bagul <223601910+tcw-sameer-bagul@users.noreply.github.com>
2025-09-03 09:56:02 +0530

    CORS added

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,14 @@
 import express, { type Request, Response, NextFunction } from "express";
+import cors from "cors";
 import { registerRoutes } from "./routes/index";
 import { setupVite, serveStatic, log } from "./vite";
 
+
 const app = express();
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
